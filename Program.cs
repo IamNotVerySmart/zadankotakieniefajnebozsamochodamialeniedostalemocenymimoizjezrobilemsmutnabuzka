322be035eb76127242eb3d62fbab05f7209987bd
IamNotVerySmart <93706080+IamNotVerySmart@users.noreply.github.com>
2023-02-22 10:05:40 +0100

    Add files via upload
    samochodziki w c płoteczek

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using bartekjestazjatom;
+
+
+string[] lines = File.ReadAllLines("C:\\Users\\admin\\source\\repos\\wsumietonwmcoalejakieszadankochybanaocenealeniejestempewien\\wsumietonwmcoalejakieszadankochybanaocenealeniejestempewien\\samochody.txt");
+Samochod[] Samochody = new Samochod[lines.Length];
+int i = 0;
+    foreach(string line in lines)
+    {
+        string[] entries = line.Split(' ');
+        Samochody[i] = new Samochod(entries[0], entries[1], double.Parse(entries[2], System.Globalization.CultureInfo.InvariantCulture), int.Parse(entries[3]));
+        i++;
+    }
+    foreach(Samochod samochod in Samochody)
+    {
+        Console.WriteLine(samochod.ToString());
+    }
+    
+
+    
---
+++ samochod.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace bartekjestazjatom
+{
+    internal class Samochod
+    {
+        string marka, model;
+        double pojemnosc;
+        int moc;
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public double Pojemnosc
+        {
+            get { return pojemnosc; }
+            set { pojemnosc = value; }
+        }
+        public int Moc
+        {
+            get { return moc; }
+            set { moc = value; }
+        }
+        public Samochod()
+        {
+            marka = "";
+            model = "";
+            pojemnosc = 0;
+            moc = 0;
+        }
+        public Samochod(string Marka, string Model, double Pojemnosc, int Moc)
+        {
+            this.marka = Marka;
+            this.model = Model;
+            this.pojemnosc = Pojemnosc;
+            this.moc = Moc;
+        }
+        public Samochod(Samochod s)
+        {
+            marka = s.Marka;
+            model = s.Model;
+            pojemnosc = s.Pojemnosc;
+            moc = s.Moc;
+        }
+        public void Copy(Samochod s)
+        {
+            marka = s.Marka;
+            model = s.Model;
+            pojemnosc = s.Pojemnosc;
+            moc = s.Moc;
+        }
+        public override string ToString()
+        {
+            return "Marka: " + marka + " Model: " + model + " "+ pojemnosc + "L" + moc + "KM";
+        }
+    }
+}
